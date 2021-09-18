b4afffe4525521d309b35bf74e2250d3f43565d4
moisyo <aranjueznoda@gmail.com>
2021-09-18 21:12:45 +0900

    fixed

---
+++ .history/src/components/PageHead/index_20210918202639.jsx
@@ -0,0 +1,25 @@
+// components/PageHead/index.jsx
+
+import Head from 'next/head';
+
+export const PageHead = () => {
+  return (
+    <Head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#'>
+      <meta name='viewport' content='width=device-width,initial-scale=1.0' />
+      <title>HOME | 私のポートフォリオサイト</title>
+      <meta name='description' content='N-create 私のポートフォリオサイト' />
+      <meta property='og:title' content='HOME | 私のポートフォリオサイト' />
+      <meta property='og:description' content='N-create 私のポートフォリオサイト' />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content='https://example.com' />
+      <meta property='og:image' content='https://example.com/ogp.png' />
+      <meta property='og:site_name' content='私のポートフォリオサイト' />
+
+      {/* Facebook */}
+      <meta property='fb:app_id' content='Facebookのapp_idを入力' />
+
+      {/* Twitter */}
+      <meta name='twitter:card' content='summary' />
+    </Head>
+  );
+};
