ff566bd300e616bdd8d6175c34e3755396133e6d
moisyo <aranjueznoda@gmail.com>
2021-09-20 17:22:26 +0900

    9-20

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,6 +6,10 @@ export const Contact = () => {
   return (
     <section id='contact' className='p-8'>
       <SectionTitle title='お仕事のご相談' lead='CONTACT' position='center' color='gray-700y' />
+      <div className='flex lg:my-8 sm:my-4'>
+        <h3 className='lg:text-base sm:text-sm'>mail</h3>
+        <p className='lg:text-base sm:text-sm'>n.create39@gmail.com</p>
+      </div>
     </section>
   );
 };
